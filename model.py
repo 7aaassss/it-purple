b962677ee9ab5975cfa515b1a94760184e030abc
art2m <91477956+7aaassss@users.noreply.github.com>
2024-03-12 21:43:07 +0300

    Delete model.py poshel naxuy

--- model.py
+++
@@ -1,8 +0,0 @@
-#from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-
-#tokenizer = AutoTokenizer.from_pretrained("sambanovasystems/SambaLingo-Russian-Chat", use_fast=False)
-#model = AutoModelForCausalLM.from_pretrained("sambanovasystems/SambaLingo-Russian-Chat", device_map="auto", torch_dtype="auto")
-#pipe = pipeline("text-generation", model="ai-forever/mGPT")
-
-
-
