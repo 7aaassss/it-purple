63380498b7e33bbd4afa80323d9e646830e3e24c
Maksim Litvinov <95618433+maksimlitvinov39kg@users.noreply.github.com>
2024-03-13 11:13:17 +0300

    Update pdf2text.py

--- pdf2text.py
+++ pdf2text.py
@@ -5,9 +5,10 @@ import io
 def extract_text_from_pdf_url(url):
     response = requests.get(url)
     final_text= ''
+    success = True
     if response.status_code == 200:
         with io.BytesIO(response.content) as f:
-            try: 
+            try:
                 with pdfplumber.open(f) as pdf:
                     for page_num, page in enumerate(pdf.pages):
                         text = page.extract_text()
@@ -17,4 +18,7 @@ def extract_text_from_pdf_url(url):
                             final_text+=text
             except Exception as e:
                 print(e)
-    return final_text
+                success = False  # Операция не удалась
+    else:
+        success = False  # Ответ сервера не 200
+    return final_text, success
