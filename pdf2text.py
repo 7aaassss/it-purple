cfb73c9c34ecb3f464da2116788dd57771b0e268
art2m <91477956+7aaassss@users.noreply.github.com>
2024-03-14 09:26:39 +0300

    Added comms

--- pdf2text.py
+++ pdf2text.py
@@ -4,13 +4,13 @@ import io
 
 
 
-def extract_text_from_pdf_url(url):
-    response = requests.get(url)
+def extract_text_from_pdf_url(url): # функция для конвертации пдф файлов в текст
+    response = requests.get(url) # запрос по ссылке 
     final_text= ''
     pages= {}
-    success = True
+    success = True # флаг (в дальнейшем для добавления файла в бд)
     if response.status_code == 200:
-        with io.BytesIO(response.content) as f:
+        with io.BytesIO(response.content) as f: # битовойе открытие файла
             try:
                 with pdfplumber.open(f) as pdf:
                     for page_num, page in enumerate(pdf.pages):
@@ -18,8 +18,8 @@ def extract_text_from_pdf_url(url):
                         #print("Страница", page_num + 1)
                         if text:
                         # print(text)
-                            final_text+=text
-                            pages.setdefault(page_num + 1, text)
+                            final_text+=text # конкантенация текста со всех страниц
+                            pages.setdefault(page_num + 1, text) #дополнительны словарь вида (страница : текст страницы)
             except Exception as e:
                 print(e)
                 success = False  # Операция не удалась
